1a801ff6da87015e6f3bdbe9f913c93de4be8cc5
JKK86 <j.kurowski86@wp.pl>
2021-11-09 17:37:35 +0100

    Add ImageUpload model and define media settings

--- images_app/models.py
+++ images_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from upload_images_REST_API import settings
+
+
+def image_directory_path(instance, filename):
+    return f'images/{filename}'
+
+
+class ImageUpload(models.Model):
+    title = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128)
+    image = models.ImageField(upload_to=image_directory_path)
+    alt = models.TextField(null=True)
+    uploaded = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='uploaded_images')
